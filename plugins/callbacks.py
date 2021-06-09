3999084c1c4c57459c661ed89edacf4c55268666
levi749 <84772173+levi749@users.noreply.github.com>
2021-06-09 08:37:38 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy?", url="https://t.me/Levinomad_TG"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("GROUP", url="https://t.me/cl_chatgrp"),
+                    InlineKeyboardButton("CHANNEL", url="https://t.me/CLALL1")
                 ]
             ]
         )
